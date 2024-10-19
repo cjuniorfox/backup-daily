47a56c2ae71af7c17dcf05678e5fca3343029e92
Junior_FOX <cjuniorfox@gmail.com>
2024-10-19 11:37:13 -0300

    heavly refactored routine to address some issues

--- opt/backup-daily/backup.py
+++ opt/backup-daily/backup.py
@@ -1,16 +1,26 @@
 #!/bin/python3
 
-import os,subprocess,re,time, socket
+import sys, os,subprocess,re,time, socket, logging
 from datetime import datetime
 from glob import glob
 from operator import contains
+import argparse
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(os.path.join('/var','log','backup.log')),
+        logging.StreamHandler()
+    ]
+)
 
 def get_fs_type(path):
     try:
         fstype = subprocess.check_output(['df', '--output=fstype', path], stderr=subprocess.PIPE).decode().split('\n')[1]
         return fstype.strip()
     except Exception as e:
-        print(f"Error determining filesystem type: {e}")
+        logging.exception('Error getting filesystem type "%s"',e.message)
         return None
 
 def zfs_check_tag(tag_name):
@@ -35,36 +45,43 @@ def btrfs_check_tag(tag_name):
     except subprocess.CalledProcessError:
         None
 
-def from_tag_incremental(name,file_title,directory):
-    files = glob(os.path.join( directory, f'{file_title}*.{fs_type}.gz'))
+def from_tag_incremental(name,file_title,dest_path):
+    files = glob(os.path.join( dest_path, f'{file_title}*.{fs_type}.gz'))
     if len(files) == 0:
-        return {'tag': '', 'file_name' : ''}
+        return None
     files.sort()
     files.reverse()
     for i in files:
         tag=i.split("@")[1].replace(".incremental","").replace(f'.{fs_type}.gz',"")
         tag_name= f'{name}@{tag}' if fs_type=='zfs' else f'{file_title}@{tag}'
-        if glob(os.path.join(directory,f'{file_title}@{tag}.doing.txt')) :
-            return {'tag':'','file_name':''}
+        if glob(os.path.join(dest_path,f'{file_title}@{tag}.doing.txt')) :
+            return {'tag':'','source_file':''}
         snapshot = zfs_check_tag(tag_name) if is_zfs else btrfs_check_tag(tag_name)
         if snapshot is not None and len(snapshot) > 0:
-            return {'tag' : snapshot, 'file_name': i }
-    return {'tag': '', 'file_name' : ''}
+            return {'tag' : snapshot, 'source_file': i }
+    return None
 
 def snapshot_data(name):
     file_title=name.replace('/','--')
     formatted_date = datetime.now().strftime("%Y-%m-%d_T%H-%M-%S")
     file_name = f'{file_title}@{formatted_date}.{fs_type}.gz'
     file_name_incremental = file_name.replace(fs_type,f'incremental.{fs_type}')
-    directory = os.path.join(path,file_title)
-    from_tag_incr=from_tag_incremental(name,file_title,directory)
+    dest_path = os.path.join(DEST_PATH,file_title)
+    from_tag=from_tag_incremental(name,file_title,dest_path)
+
+    incremental = {
+        "file_name" : file_name_incremental,
+        "target" : os.path.join(dest_path,file_name_incremental),
+        "from_tag" : from_tag
+    } if from_tag is not None and from_tag.get("tag") != "" else {}
+
     return {
             "file_title" : file_title, 
             "tag" : f'{name}@{formatted_date}' if is_zfs else f'{file_title}@{formatted_date}',
             "file_name" : file_name,
-            "file_name_incremental" : file_name_incremental if from_tag_incr != None and from_tag_incr != "" else "",
-            "from_tag_incremental": from_tag_incr,
-            "directory" : directory,
+            "incremental" : incremental,
+            "dest_path" : dest_path,
+            "target" : os.path.join(dest_path,file_name),
     }
 
 def mountpoint_data(i):
@@ -107,11 +124,12 @@ def btrfs_list():
                 #Check if subvolume is mounted
                 if mountpoint_match and 'tmp' not in mountpoint_match[0] and 'snap' not in mountpoint_match[0]:
                     mountpoint=mountpoint_match[0].split(' ')[2]
-                    writable_subvolumes.append(mountpoint_data(f'{path} {mountpoint} yes'.split()))
+                    mp_data = mountpoint_data(f'{path} {mountpoint} yes'.split())
+                    writable_subvolumes.append(mp_data)
         
         return writable_subvolumes
     except subprocess.CalledProcessError as e:
-        print(f"Error listing BTRFS subvolumes: {e}")
+        logging.exception('Error listing BTRFS subvolumes "%s"', e.args[0])
         return []
 
 def take_zfs_snapshot(snapshot_tag):
@@ -120,6 +138,7 @@ def take_zfs_snapshot(snapshot_tag):
         _, _ = snap.communicate()
         return snap.returncode == 0
     except subprocess.CalledProcessError:
+        logging.exception('Error taking snapshot "%s"', snapshot_tag)
         return False
 
 def take_btrfs_snapshot(snapshot_tag,snap_subvol,mountpoint):
@@ -129,104 +148,140 @@ def take_btrfs_snapshot(snapshot_tag,snap_subvol,mountpoint):
             proc = subprocess.Popen(['btrfs','subvolume','create',snap_subvol])
             _, _ = proc.communicate()
         except subprocess.CalledProcessError:
-            print(f'Error creating .snap subvolume for {snapshot_tag}')
+            logging.exception('Error creating .snap subvolume for "%s"', snapshot_tag)
             return False
     try:
         proc = subprocess.Popen(['btrfs','subvolume','snapshot','-r',mountpoint,os.path.join(snap_subvol,snapshot_tag)])
         _, _ = proc.communicate()
         return proc.returncode == 0
     except subprocess.CalledProcessError:
-        print(f'Error creating snapshot for {snapshot_tag}')
-        return False
+        logging.exception('Error creating .snap subvolume for "%s"', snapshot_tag)
     return False
         
 def take_snapshot(i):
     snapshot_tag= i['snapshot']['tag']
     snap_subvol=i['snap']
     mountpoint=i['mountpoint']
-    print(f'Creating snapshot "{snapshot_tag}"')
+    logging.info('Taking snapshot for "%s"', snapshot_tag)
     take_snapshot = take_zfs_snapshot(snapshot_tag) if is_zfs else take_btrfs_snapshot(snapshot_tag,snap_subvol,mountpoint)
     if take_snapshot:
+        logging.debug('Snapshot "%s" successfully taken', snapshot_tag)
         return True
     else:
-        print(f'ERROR While taking snapshot "{snapshot_tag}"')
+        logging.error('ERROR While taking snapshot "%s"', snapshot_tag)
         return False
     
 
 def send_backup_using_bash(i,incremental=False):
-    snap=i['snapshot']
-    tag=snap['tag']
-    snapshot_dir=i['snap']
+    snap=i.get('snapshot')
+    tag=snap.get('tag')
+    snapshot_dir=i.get('snap')
     
-    from_tag_incremental=snap['from_tag_incremental']['tag']
-    dest_file=os.path.join(snap['directory'],(snap['file_name_incremental'] if incremental else snap['file_name'] ))
+    from_tag_incremental=snap.get('incremental',{}).get('from_tag',{}).get('tag')
+    dest_file = snap.get('target')
+    if incremental and snap.get('incremental',{}).get('target') is not None:
+        dest_file = snap.get('incremental',{}).get('target')
     doing_file=dest_file.replace(".incremental","").replace(f'.{fs_type}.gz',".doing.txt")
     incr_txt_file=dest_file.replace(f'.{fs_type}.gz',".txt")
-
-    btrfs_snapshot=os.path.join(snapshot_dir,tag)
-    btrfs_snapshot_incr=os.path.join(snapshot_dir,from_tag_incremental)
     
     with open(doing_file,'w') as f:
-        f.write("writing file")
-    if incremental:
-        with open(incr_txt_file,"w") as f:
-            f.write(snap['from_tag_incremental']['file_name'])
+        f.write("writing file")        
+    
+    btrfs_snapshot=os.path.join(snapshot_dir,tag)
+
     zfs_cmd=f'zfs send {tag} | pv -B 512M | pigz -c > {dest_file}'
-    zfs_cmd_incr=f'zfs send -i {from_tag_incremental} {tag} | pv -B 512M | pigz -c > {dest_file}'
     btrfs_cmd=f'btrfs send "{btrfs_snapshot}" | pv -B 512M | pigz -c > {dest_file}'
-    btrfs_cmd_incr=f'btrfs send -p "{btrfs_snapshot_incr}" "{btrfs_snapshot}" | pv -B 512M | pigz -c > {dest_file}'
+    if (incremental and from_tag_incremental is not None and from_tag_incremental != ''):
+        with open(incr_txt_file,"w") as f:
+            f.write(dest_file)
+        logging.info('Sending snapshot tag "%s" to "%s" incrementally from "%s".',tag,dest_file,from_tag_incremental)
+        zfs_cmd=f'zfs send -i {from_tag_incremental} {tag} | pv -B 512M | pigz -c > {dest_file}'
+        btrfs_cmd=f'btrfs send -p "{btrfs_snapshot_incr}" "{btrfs_snapshot}" | pv -B 512M | pigz -c > {dest_file}'
+    else:
+        logging.info('Sending snapshot tag "%s" to "%s".',tag,dest_file)
     try:
         if is_zfs:
-            zfs=subprocess.Popen(['bash','-c',zfs_cmd_incr if incremental else zfs_cmd])
+            zfs=subprocess.Popen(['bash','-c',zfs_cmd])
             zfs.wait()
         else:
-            btrfs=subprocess.Popen(['bash','-c',btrfs_cmd_incr if incremental else btrfs_cmd])
+            btrfs=subprocess.Popen(['bash','-c',btrfs_cmd])
             btrfs.wait()
         if os.path.exists(doing_file):
             os.remove(doing_file)
     except subprocess.CalledProcessError:
-        print (f'Error doing the backup of {tag} for {dest_file}')
+        logging.exception('Error doing the backup of "%s" for "%s"', tag, dest_file)
         
 def backup(i):
-    os.makedirs(i['snapshot']['directory'], exist_ok=True)
-    if i['snapshot']['from_tag_incremental']['tag'] != '':
-        print("Sending snapshot " + i['snapshot']['tag'] + ' to the file ' + i['snapshot']['file_name_incremental'])
+    logging.info('Starting backup of "%s"', i['snapshot']['tag'])
+    logging.debug('Creating directory: "%s"', i['snapshot']['tag'])
+    os.makedirs(i.get('snapshot').get('dest_path'), exist_ok=True)
+    incremental_tag = i.get('incremental',{}).get('from_tag',{}).get('tag')
+    if incremental_tag is not None and incremental_tag != '':
         send_backup_using_bash(i,True)
     else:
-        print("Sending snapshot " + i['snapshot']['tag'] + ' to the file ' + i['snapshot']['file_name'])
         send_backup_using_bash(i)
 
 def do_the_job(fs_list):
+    logging.info('Starting backup job for "%s"', workname)
     for i in fs_list:
         if take_snapshot(i):
             time.sleep(0.5)
             backup(i)
 
-def mount_shares(block_device,mountpoint):
+def mount_shares(block_device: str, mountpoint: str, options: str) -> bool:
     try:
-        mount = subprocess.Popen(['mount',block_device,mountpoint])
+        if not os.path.exists(mountpoint):
+            logging.info('Mountpoint "%s" does not exist. Creating it.', mountpoint)
+            os.makedirs(mountpoint)
+        command = ['mount', block_device, mountpoint]
+        if options:
+            command.extend(f'-o,{options}')
+        logging.info('Mounting backup "%s" at mountpoint "%s" with options "%s"', block_device, mountpoint, options)
+        mount = subprocess.Popen(command)
         mount.wait()
-        return True
+        if mount.returncode == 0:
+            return True
+        else:
+            logging.error(f'ERROR: Mount command failed with return code {mount.returncode}')
+            return False
     except subprocess.CalledProcessError as e:
-        print(f'ERROR While mounting backup {block_device} at mountpoint "{mountpoint}"')
-        print(f'Error: {e}')
+        logging.exception(f'Error while mounting backup {block_device} at mountpoint "{mountpoint}"')
+        logging.debug(f'Error: {e}')
         return False
 
 def umount_shares(mountpoint):
+    logging.info('Unmounting backup at mountpoint "%s"', mountpoint)
     umount = subprocess.Popen(['umount',mountpoint])
     umount.wait()
+    if umount.returncode == 0:
+        return True
+    else:
+        logging.error(f'ERROR: Umount command failed with return code {umount.returncode}')
+        return False
 
 if __name__ == '__main__':
     block_device='lacie-d2.local:/srv/Files'
-    #block_device='/dev/disk/by-uuid/97b983bc-ba92-430d-ae61-7f212bf0969a'
     mountpoint='/mnt'
     options=''
     workname = socket.gethostname()
 
     fs_type = get_fs_type("/")
     is_zfs = fs_type == 'zfs'
-    if mount_shares(block_device,mountpoint):
-        path=os.path.join(mountpoint,workname)
-        #print(zfs_list() if is_zfs else btrfs_list())
-        do_the_job(zfs_list() if is_zfs else btrfs_list())
-        umount_shares(mountpoint)
+    parser = argparse.ArgumentParser(description='Backup script for ZFS and BTRFS filesystems.')
+    parser.add_argument('--print-fs-list', '-p', action='store_true', help='Does not do the backup. Just print the list of filesystems to backup.')
+    parser.add_argument('--block-device','-b', type=str, required=True, help='Backup script for ZFS and BTRFS filesystems.')
+    parser.add_argument('--mountpoint','-m', type=str, required=True, help='Mountpoint where the backup will be stored.')
+    parser.add_argument('--options','-o', type=str, required=False, help='Mounter options.')
+    args = parser.parse_args()
+    mountpoint = args.mountpoint
+    options = args.options
+    block_device = args.block_device
+    print_fs_list = args.print_fs_list
+    DEST_PATH=os.path.join(mountpoint,workname)
+    if mount_shares(block_device,mountpoint, options):
+        if print_fs_list:
+            fs_list = zfs_list() if is_zfs else btrfs_list()
+            print(str(fs_list).replace("'",'"'))
+        else:
+            do_the_job(zfs_list() if is_zfs else btrfs_list())
+        sys.exit(0 if umount_shares(mountpoint) else 1)
